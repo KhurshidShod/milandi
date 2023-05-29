d0a74d8db52d97c86f1c924742d1aff8be3e0a42
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-05-29 21:41:11 +0500

    header and footer readyu

--- js/main.js
+++ js/main.js
@@ -5,5 +5,12 @@ navOpen.addEventListener("click", () => {
     document.querySelector(".nav__menu").style.top = "0%"
 });
 navClose.addEventListener("click", () => {
-    document.querySelector(".nav__menu").style.top = "-150%"
+    document.querySelector(".nav__menu").style.top = "-1500%"
 });
+window.addEventListener("scroll", () => {
+    if(pageYOffset >= 100){
+        document.querySelector("header").classList.add("shrink")
+    } else {
+        document.querySelector("header").classList.remove("shrink")
+    }
+})
