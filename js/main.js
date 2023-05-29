0612e257827f8f90bea352826f51f24c14172935
Xurshidbek Shodiyev <85980781+KhurshidShod@users.noreply.github.com>
2023-05-29 18:59:00 +0500

    change

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const navOpen = document.getElementById("navOpen");
+const navClose = document.getElementById("navClose");
+
+navOpen.addEventListener("click", () => {
+    document.querySelector(".nav__menu").style.top = "0%"
+});
+navClose.addEventListener("click", () => {
+    document.querySelector(".nav__menu").style.top = "-150%"
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@
 module.exports = {
   content: ['*.html'],
   theme: {
+    screens: {
+      'sm': '567px',
+      'md': '768px',
+      'lg': '1146px',
+      'xl': '1146px'
+    },
     extend: {
       colors: {
         black: '#000000',
         navBlack: '#0D0D0D',
         yellow: '#FFC000',
+        navBg: '#FAFAFA',
         lightGrey: '#E5E5E5',
+        voytiBtnBg: 'rgba(0, 0, 0, 0.05);',
         blueBg: 'radial-gradient(100% 1071.07% at 0% 0%, #0F0D2B 0%, #0C0926 100%);'
       },
       fontSize: {
@@ -17,6 +25,9 @@ module.exports = {
         font16: '16px',
         font23: '23px',
         font34: '34px',
+      },
+      container: {
+        maxWidth: '500px'
       }
     },
   },
