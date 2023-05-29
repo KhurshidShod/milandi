7cb020ecb06651fbc209273d14cc30de1b3dd1d7
NeAlfa <sharipovdevelopper@gmail.com>
2023-05-29 07:11:21 -0700

    as

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-// Initialization for ES Users
-import {
-  Collapse,
-  initTE,
-} from "tw-elements";
-
-initTE({ Collapse });
-=======
 const navOpen = document.getElementById("navOpen");
 const navClose = document.getElementById("navClose");
 
@@ -16,4 +7,3 @@ navOpen.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
     document.querySelector(".nav__menu").style.top = "-150%"
 });
->>>>>>> 0612e257827f8f90bea352826f51f24c14172935
